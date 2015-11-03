35b0970c485c6a4ae6d458a2ce00f5490f4e2f2b
shu <shu@w10pc>
2015-11-03 18:02:51 +0000

    second commit

---
+++ src/Message.java
@@ -0,0 +1,11 @@
+
+public class Message {
+
+	public static String hello = "Hello World";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(hello);
+	}
+
+}
